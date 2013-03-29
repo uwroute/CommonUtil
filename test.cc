d4ed23afc4bc380988cfee972232f28941602163
uwroute <liuxun001@126.com>
2013-03-29 23:27:42 +0800

    add test

---
+++ test.cc
@@ -0,0 +1,10 @@
+// Copyright (C) 2012 Jike Inc. All Rights Reserved
+// Author: liuxun@jike.com (Xun Liu)
+//
+// test.cc
+
+#include <iostream>
+
+void main(){
+  std::cout << "Hello World!" << std::endl;
+}
